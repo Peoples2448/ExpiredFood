ce2a270f39cd11f61917941fff7da562fd574cf5
Yutong Zhu <lbwfeichangnb@gmail.com>
2023-03-25 15:13:59 -0400

    deleted codes from create react app, add sign-in component, login-in component, and also a auth page is intended to use for react router

---
+++ src/Component/login/login.component.jsx
@@ -0,0 +1,16 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>User Login</h1>
+            <form>
+                <label>Username</label>
+                <input type='text' />
+                <label>Password</label>
+                <input label="Password" type='password' />
+                <button />
+            </form>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/Component/sign-in/sigin-in.component.jsx
@@ -0,0 +1,20 @@
+const SignIn = () => {
+    return (
+        <div>
+            <h1>User Login</h1>
+            <form>
+                <label>Email</label>
+                <input type="email"/>
+                <label>Username</label>
+                <input type='text' />
+                <label>Password</label>
+                <input label="Password" type='password' />
+                <label>Password</label>
+                <input label="Password" type='password' />
+                <button />
+            </form>
+        </div>
+    )
+}
+
+export default SignIn;
