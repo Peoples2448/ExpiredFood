d9afecc1573de943c297f438936044d7351fa403
Yutong Zhu <lbwfeichangnb@gmail.com>
2023-03-25 17:27:34 -0400

    installed react router dom and added react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import AuthPage from "./Router/auth-page/auth-page.router";
 import './App.css'
+import { Route, Routes } from "react-router-dom";
 function App() {
   return(
-    <AuthPage/>
+    <Routes>
+      <Route>
+      <Route index={true} element={<AuthPage/>}/>      
+      <Route path="/products" ></Route>
+      </Route>
+    </Routes>
   )
 }
 
